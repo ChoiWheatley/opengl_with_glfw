7b05c72cb4f8d276e971ca990b139fa809cfa502
ChoiWheatley <chltmdgus604@naver.com>
2022-02-18 00:52:54 +0900

    change test setup

--- Camera/main.cpp
+++ Camera/main.cpp
@@ -2,9 +2,9 @@
 #include "headers.h"
 extern int main_camera(int argc, char** argv);
 extern int use_camera_class_main(int argc, char** argv);
-extern int test_image_texture_main(int argc, char ** argv);
+extern int test_main(int argc, char ** argv);
 
 int main(int argc, char** argv)
 {
-	return test_image_texture_main(argc, argv);
+	return test_main(argc, argv);
 }
--- Camera/test.cpp
+++ Camera/test.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include "Image.h"
 #include "Stub.h"
+
+static bool image_test();
 /**
  * \brief Image 객체가 올바르게 데이터를 로드하는가?
  * \return 0=올바르게 로드한다. 1=올바르게 로드하지 못한다.
  */
-int test_image_texture_main(int argc, char ** argv)
+int test_main(int argc, char ** argv)
 {
-	Image img{Stub::Image::filename};
+	if (image_test())
+		return 1;
+	return 0;
+}
+
+bool image_test()
+{
+	const Image img{Stub::Image::filename};
 	if (!img.data)
 	{
-		return 1;
+		return false;
 	}
 	std::cerr << "nrChannel = " << img.nrChannels << '\n';
 	std::cerr << "(width, height)= (" << img.width << ", " << img.height << ")\n";
 
-	return 0;
+	return true;
 }
