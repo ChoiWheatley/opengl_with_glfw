6ab532d3b177489305afb0ce7f0eac7f0f1d4338
ChoiWheatley <chltmdgus604@naver.com>
2022-02-25 23:11:52 +0900

    PosAxisAngle derived from Matrix4

---
+++ Camera/PosAxisAngle.cpp
@@ -0,0 +1,31 @@
+﻿#include "PosAxisAngle.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+glm::mat4 PosAxisAngle::getMatrix() const
+{
+	glm::mat4 ret{1.f};
+	ret = glm::translate(ret, pos);
+	ret = glm::rotate(ret, angle, axis);
+	return ret;
+}
+
+void PosAxisAngle::translate(const glm::vec3& vec)
+{
+	pos += vec;
+}
+
+void PosAxisAngle::rotate(const float& deltaAngle)
+{
+	angle += deltaAngle;
+}
+
+void PosAxisAngle::operator+(const PosAxisAngle& paa)
+{
+	pos += paa.pos;
+	axis += paa.axis;
+	angle += paa.angle;
+}
+
+PosAxisAngle::PosAxisAngle(glm::vec3 pos, glm::vec3 axis, const float angle)
+	:pos{ pos }, axis{ axis }, angle{ angle }
+{}
---
+++ Camera/PosAxisAngle.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Matrix4.h"
+
+struct PosAxisAngle final : public Matrix4
+{
+public: // interface
+	glm::mat4 getMatrix() const override;
+	void translate(const glm::vec3& vec);
+	void rotate(const float& deltaAngle);
+	void operator+(const PosAxisAngle& paa);
+public: // constructor
+	PosAxisAngle(glm::vec3 pos, glm::vec3 axis, const float angle);
+	~PosAxisAngle() override = default;
+public: // member
+	glm::vec3 pos;
+	glm::vec3 axis;
+	float angle;
+};
